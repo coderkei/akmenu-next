18616c045da5914c4cb6e67d48d3a658a380ab7c
Kei <7037851+coderkei@users.noreply.github.com>
2026-05-12 17:10:09 +0100

    Update version.h

--- arm9/source/version.h
+++ arm9/source/version.h
@@ -8,7 +8,7 @@
 */
 
 #define AKMENU_VERSION_MAIN "2"
-#define AKMENU_VERSION_SUB "2.0"
+#define AKMENU_VERSION_SUB "2.1"
 
 #define AKMENU_LOADER_NAME "nds-bootstrap"
 #define AKMENU_PICO_NAME "Pico-Loader"
